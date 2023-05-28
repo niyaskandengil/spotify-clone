6ce5d1da340a9b2ade420bf35b26314d2d2495b1
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-29 00:28:20 +0530

    #

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import {IoLibrary} from 'react-icons/io5';
+import {mdHomeFilled, MdSearch} from 'react-icons/md';
 
 export default function Sidebar() {
   return (
     <Container>
       <div className="top__links">
         <div className="logo">
-            
+           <img src='https://i.ibb.co/Bg8P8wQ/Spotify-Logo-White.png' 
+           alt='spotify'/>
         </div>
+        <ul>
+            <li>
+                <mdHomeFilled/>
+                <span>Home</span>
+            </li>
+
+            <li>
+                <MdSearch/>
+                <span>Search</span>
+            </li>
+            <li>
+                <IoLibrary/>
+                <span>Your Library</span>
+            </li>
+        </ul>
       </div>
     </Container>
   )
