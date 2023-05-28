23997c6bd31471ce34ce9d763179fd4f320b1318
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-28 23:38:58 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react'
 import Login from './components/Login'
+import { useStateProvider } from './utils/StateProvider';
+import { reducerCases } from './utils/Constants';
 
 export default function App() {
+    const [{token},dispatch] = useStateProvider();
     useEffect(() => {
         const hash = window.location.hash;
         if(hash)
         {
             const token = hash.substring(1).split("&")[0].split("=")[1];
-            console.log(token);
+            dispatch({action:reducerCases.SET_TOKEN , token})
         };
-    },[])
+    },[token,dispatch])
   return (
     <div>
       <Login />
