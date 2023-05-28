941e6aa3a042a5f16cbd4c9228842ada85d328b9
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-28 22:51:18 +0530

    $

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,10 +4,25 @@ import styled from 'styled-components';
 export default function Login() {
   return (
     <Container>
-      <img src='https://i.ibb.co/gtfJvBy/Spotify-Logo-Black.png'/>
+      <img src='https://i.ibb.co/gtfJvBy/Spotify-Logo-Black.png' alt='spotify'/>
       <button>Connect with Spotify</button>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height:100vh;
+  width:100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img{
+    height: 20vh;
+    button{
+      padding: 1rem 5rem;
+      border-radius: 99px;
+        
+    `;
