4a839512bbfcf566aa0e6595c17cc5bfdebd58b1
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-28 22:32:53 +0530

    #

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App' 
 import './index.css'
+import { StateProvider } from './utils/StateProvider';
+import reducer, { initialState } from './utils/reducer';
 
 ReactDOM.render(
-   <App />, 
+    <React.StrictMode>
+        <StateProvider initialState={initialState} reducer={reducer}>
+            <App />
+        </StateProvider>
+    </React.StrictMode>, 
    document.getElementById('root')
 );
