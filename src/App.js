53c67b5eea5163087587142e13180f3ae9704583
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-28 23:27:19 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Login'
 
 export default function App() {
+    useEffect(() => {
+        const hash = window.location.hash;
+        if(hash)
+        {
+            const token = hash.substring(1).split("&")[0].split("=")[1];
+            console.log(token);
+        };
+    },[])
   return (
     <div>
-      <Login/>
+      <Login />
     </div>
   )
 }
