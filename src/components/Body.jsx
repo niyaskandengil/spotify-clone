e3a2e5c535d4f31abf4a8fe35f7f9bfed05f0585
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-06-11 12:58:12 +0530

    #

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,31 +1,46 @@
-import React,{useEffect} from 'react'
-import styled from 'styled-components';
-import {AiFillClockCircle} from 'react-icons/ai';
-import { useStateProvider } from '../utils/StateProvider';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { AiFillClockCircle } from "react-icons/ai";
+import { useStateProvider } from "../utils/StateProvider";
+import axios from "axios";
 
 export default function Body() {
-  const[{token,selectedPlaylistId}, dispatch]=useStateProvider();
+  const [{ token, selectedPlaylistId }, dispatch] = useStateProvider();
 
   useEffect(() => {
     const getIntialPlaylist = async () => {
-      const response = await axios.get(`https://api.spotify.com/v1/playlists/${
-        (selectedPlaylistId,
-        { 
-          headers:{
+      const response = await axios.get(
+        `https://api.spotify.com/v1/playlists/${selectedPlaylistId}`,
+        {
+          headers: {
             Authorization: "Bearer " + token,
             "Content-Type": "application/json",
-        },
-      })
-    }`
-    );
-  };
-  },[token,dispatch])
-  return (
-    <Container>
-      Body 122
-    </Container>
-  )
+          },
+        }
+      );
+      const selectedPlaylist = {
+        id: response.data.id,
+        name: response.data.name,
+        describtion: response.data.description.startsWith("<a") 
+           ? "" : response.data.description,
+        image: response.data.images[0].url,
+        tracks: response.data.tracks.items.map(({track}) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists.map((artist) => artist.name),
+          image: track.album.images[2].url,
+          duration: track.duration_ms,
+          album: track.album.name,
+          context_uri: track.album.uri,
+          track_number: track.track_number,
+        
+        })),
+       };
+      
+    };
+    getIntialPlaylist();
+  }, [token, dispatch]);
+  return <Container>Body 122</Container>;
 }
 
 const Container = styled.div``;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import { useStateProvider } from '../utils/StateProvider';
 
 export default function Navbar() {
   const [{userInfo }]=useStateProvider();
-  console.log(userInfo)
   return (
     <Container>
       <div className="search__bar">
