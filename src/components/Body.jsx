42cb59d2b53418c4dde6fd5b28aa1bde3a98552c
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-06-11 14:45:10 +0530

    #

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,7 +9,7 @@ export default function Body() {
   const [{ token, selectedPlaylistId, selectedPlaylist }, dispatch] = useStateProvider();
 
   useEffect(() => {
-    const getIntialPlaylist = async () => {
+    const getInitialPlaylist = async () => {
       const response = await axios.get(
         `https://api.spotify.com/v1/playlists/${selectedPlaylistId}`,
         {
@@ -23,7 +23,8 @@ export default function Body() {
         id: response.data.id,
         name: response.data.name,
         describtion: response.data.description.startsWith("<a") 
-           ? "" : response.data.description,
+           ? "" 
+           : response.data.description,
         image: response.data.images[0].url,
         tracks: response.data.tracks.items.map(({track}) => ({
           id: track.id,
@@ -39,7 +40,8 @@ export default function Body() {
        };
       dispatch({ type: reducerCases.SET_PLAYLIST, selectedPlaylist });
     };
-    getIntialPlaylist();
+    getInitialPlaylist();
+    console.log(selectedPlaylist);
   }, [token, dispatch,selectedPlaylistId]);
   return <Container>
     {selectedPlaylist && (
@@ -115,4 +117,70 @@ export default function Body() {
   </Container>;
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  .playlist {
+    display: flex;
+    margin: 0 2rem;
+    align-items: center;
+    gap: 2rem;
+    .image {
+      img {
+        height: 15rem;
+        box-shadow:rgba(0, 0, 0, 0.1) 0px 25px  50px -12px;
+      }
+    }
+    .details {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      color: #e0dede;
+      .title{
+        color: #fff;
+        font-size: 4rem;
+      }
+    }
+  }
+  .list {
+    .header__row{
+      display: grid;
+      grid-template-columns: 0.3fr 3fr 2fr 0.1fr;
+      color: #dddcdc;
+      margin: 1rem 0 0 0 ;
+      position: sticky;
+      top: 15vh;
+      padding: 1rem 3rem;
+      transition: 0.3s ease-in-out;
+
+    }
+    .tracks{
+      margin: 0 2rem;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 5rem;
+      .row{
+        display: grid;
+        padding: 0.5rem 1rem;
+        grid-template-columns: 0.3fr 3.1fr 2fr 0.1fr;
+        &:hover{
+          background-color: rgba(0,0,0, 0.7);
+        }
+        .col{
+          display: flex;
+          align-items: center;
+          color: #dddcdc;
+          img{
+            height: 40px;
+          }
+        }
+        .detail{
+          display: flex;
+          gap: 1rem;
+          .info{
+            display: flex;
+            flex-direction: column;
+          }
+        }
+      }
+
+    }
+  }`;
